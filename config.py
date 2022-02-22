0ffdf5ccee5698abd945b5cc6a2762bf0bea88bc
Tyson Chan <tysonchan@protonmail.com>
2022-02-23 06:58:23 +1100

    Move hyperparameters to config

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+n_epochs = 100
+n_games_per_epoch = 100
 n_rounds_per_game = 6
+batch_size = 128
+gamma = 0.999
+eps_start = 0.9
+eps_end = 0.05
+eps_decay = 200
+target_update = 10
+replay_memory_size = 10000
--- train.py
+++ train.py
@@ -34,15 +34,6 @@ class Memory:
 
 # Hyperparameters
 # TODO: tweak
-BATCH_SIZE = 128
-GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
-TARGET_UPDATE = 10
-N_EPOCHS = 1
-N_GAMES_PER_EPOCH = 100
-N_ROUNDS_PER_GAME = 6
 
 policy_net = DQN()
 target_net = DQN()
@@ -50,14 +41,14 @@ target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
 optimizer = optim.RMSprop(policy_net.parameters())
-memory = Memory(10000)
+memory = Memory(config.replay_memory_size)
 
 steps_done = 0
 
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
+    eps_threshold = config.eps_start + (config.eps_start - config.eps_end) * math.exp(-1. * steps_done / config.eps_decay)
     steps_done += 1
     # explore (random)
     if sample > eps_threshold:
@@ -70,9 +61,9 @@ def select_action(state):
     return action
 
 def optimize_model():
-    if len(memory) < BATCH_SIZE:
+    if len(memory) < config.batch_size:
         return
-    transitions = memory.sample(BATCH_SIZE)
+    transitions = memory.sample(config.batch_size)
     batch = Transition(*zip(*transitions)) # transpose batch
 
     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool)
@@ -84,7 +75,7 @@ def optimize_model():
     
     state_action_values = policy_net(state_batch).gather(1, action_batch)
 
-    next_state_values = torch.zeros(BATCH_SIZE)
+    next_state_values = torch.zeros(config.batch_size)
     next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
@@ -103,12 +94,12 @@ def optimize_model():
 
 # main training loop
 env = Wordle()
-for e in range(N_EPOCHS):
-    for g in range(N_GAMES_PER_EPOCH):
+for e in range(config.n_epochs):
+    for g in range(config.n_games_per_epoch):
         env.reset()
         state = env.state()
         # play game
-        for score in range(N_ROUNDS_PER_GAME):
+        for score in range(config.n_rounds_per_game):
             action = select_action(state)
             reward, next_state, done = env.step(action.item())
             memory.push(state, action, next_state, reward)
@@ -118,5 +109,5 @@ for e in range(N_EPOCHS):
         # train
         loss = optimize_model()
         # update target net every N games
-        if g % TARGET_UPDATE == 0:
+        if g % config.target_update == 0:
             target_net.load_state_dict(policy_net.state_dict())
